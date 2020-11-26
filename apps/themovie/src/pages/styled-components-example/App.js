66d3efc4b4abce1efc9470598684edb05c6770da
Igor P. Armelin <igor_armelin@hotmail.com>
2020-11-26 11:47:06 -0300

    add styled-components and use example

---
+++ apps/themovie/src/pages/styled-components-example/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Title} from './styles'
+
+const App = () => {
+    return (
+        <div>
+            <Title>Olá, Styled-components!</Title>
+        </div>
+    )
+}
+
+export default App;
