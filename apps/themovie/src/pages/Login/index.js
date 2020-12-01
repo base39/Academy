ce047d7b05e34582e0fd6d60e5fe02f12d68f703
Rafael Neri <rafaelrvcn@outlook.com>
2020-12-01 14:49:13 -0300

    FE integration of login with api

--- apps/themovie/src/pages/Login/index.js
+++ apps/themovie/src/pages/Login/index.js
@@ -11,6 +11,36 @@ import {
 } from '../styled-components-example/styles';
 
 export default class Section extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: ''
+		};
+	}
+
+	emailChange = event => {
+		this.setState({ email: event.target.value });
+	};
+
+	passChange = event => {
+		this.setState({ password: event.target.value });
+	};
+
+	handleSubmit = event => {
+		fetch('http://localhost:8080/auth/login', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(this.state)
+		}).then(function (response) {
+			console.log(response);
+			return response.json();
+		});
+		event.preventDefault();
+	};
+
 	render() {
 		return (
 			<div>
@@ -28,26 +58,40 @@ export default class Section extends Component {
 						enviá-lo novamente.
 					</ParagraphStyled>
 				</div>
-				<div>
-					<AcessStyled>Nome de Usuário</AcessStyled>
+				<form>
 					<div>
-						<FieldStyled id="outlined-basic" variant="outlined" />
+						<AcessStyled>Nome de Usuário</AcessStyled>
+						<div>
+							<FieldStyled
+								id="userInput"
+								onChange={this.emailChange}
+								variant="outlined"
+							/>
+						</div>
 					</div>
-				</div>
-				<div>
-					<AcessStyled>Senha</AcessStyled>
 					<div>
-						<FieldStyled id="outlined-basic" variant="outlined" />
+						<AcessStyled>Senha</AcessStyled>
+						<div>
+							<FieldStyled
+								id="passInput"
+								onChange={this.passChange}
+								variant="outlined"
+							/>
+						</div>
 					</div>
-				</div>
-				<ContainerStyled>
-					<ButtonStyled variant="contained" color="primary">
-						Entrar
-					</ButtonStyled>
-					<LinkRouterStyled to="/reset-password">
-						Resetar a Senha
-					</LinkRouterStyled>
-				</ContainerStyled>
+					<ContainerStyled>
+						<ButtonStyled
+							variant="contained"
+							color="primary"
+							onClick={this.handleSubmit}
+						>
+							Entrar
+						</ButtonStyled>
+						<LinkRouterStyled to="/reset-password">
+							Resetar a Senha
+						</LinkRouterStyled>
+					</ContainerStyled>
+				</form>
 			</div>
 		);
 	}
