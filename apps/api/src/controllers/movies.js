907b7659428021a20193f83d5f7d70a802ea33e5
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-03 14:25:56 -0300

    prettier changes

--- apps/api/src/controllers/movies.js
+++ apps/api/src/controllers/movies.js
@@ -4,39 +4,43 @@ module.exports = () => {
 	controller = {};
 
 	controller.listMovies = async (req, res) => {
-
 		const params = req.query;
 
 		try {
-			await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`, {
-				params
-			})
-			.then(response => {
-				return res.send(response.data);
-			})
+			await axios
+				.get(
+					`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`,
+					{
+						params
+					}
+				)
+				.then(response => {
+					return res.send(response.data);
+				});
 		} catch (error) {
 			return res.send(error.message);
 		}
-
-	}
+	};
 
 	controller.getMovie = async (req, res) => {
-
 		const { id } = req.params;
 		const params = req.query;
 
 		try {
-			await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`, {
-				params
-			})
-			.then(response => {
-				return res.send(response.data);
-			})
+			await axios
+				.get(
+					`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`,
+					{
+						params
+					}
+				)
+				.then(response => {
+					return res.send(response.data);
+				});
 		} catch (error) {
 			return res.send(error.message);
 		}
-
-	}
+	};
 
 	return controller;
 };
