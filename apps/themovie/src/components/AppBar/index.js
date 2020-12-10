07eb12e467b7e02467c7adf8e72acb08d62ae598
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-10 14:52:40 -0300

    prettier changes

--- apps/themovie/src/components/AppBar/index.js
+++ apps/themovie/src/components/AppBar/index.js
@@ -36,54 +36,52 @@ export default function AppBar(props) {
 			<CssBaseline />
 			<HideOnScroll {...props}>
 				<CustomAppBar>
-					<Container disableGutters={true} >
-					<Toolbar>
-						<Box display="flex" flexGrow={1}>
-							<Button
-								color="inherit"
-								aria-controls="filmes"
-								aria-haspopup="true"
-								onMouseOver={e => setAnchorFilmes(e.currentTarget)}
-							>
-								Filmes
-							</Button>
-							<Menu
-								id="filmes"
-								anchorEl={anchorFilmes}
-								keepMounted
-								open={Boolean(anchorFilmes)}
-								onClose={handleClose}
-								onMouseLeave={handleClose}
-								MenuListProps={{ onMouseLeave: handleClose }}
-								anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-								transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-								getContentAnchorEl={null}
-							>
-								<MenuItem>
-									<NoStyleLink to="/movie">Populares</NoStyleLink>
-								</MenuItem>
-								<MenuItem>
-									<NoStyleLink to="/movie">Em cartaz</NoStyleLink>
-								</MenuItem>
-								<MenuItem>
-									<NoStyleLink to="/movie">Próximas Estreias</NoStyleLink>
-								</MenuItem>
-								<MenuItem>
-									<NoStyleLink to="/movie">Mais bem avaliados</NoStyleLink>
-								</MenuItem>
-							</Menu>
-						</Box>
-						<NoStyleLink to="/login">
-							<Button color="inherit">Entrar</Button>
-						</NoStyleLink>
-					</Toolbar>
+					<Container disableGutters={true}>
+						<Toolbar>
+							<Box display="flex" flexGrow={1}>
+								<Button
+									color="inherit"
+									aria-controls="filmes"
+									aria-haspopup="true"
+									onMouseOver={e => setAnchorFilmes(e.currentTarget)}
+								>
+									Filmes
+								</Button>
+								<Menu
+									id="filmes"
+									anchorEl={anchorFilmes}
+									keepMounted
+									open={Boolean(anchorFilmes)}
+									onClose={handleClose}
+									onMouseLeave={handleClose}
+									MenuListProps={{ onMouseLeave: handleClose }}
+									anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+									transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+									getContentAnchorEl={null}
+								>
+									<MenuItem>
+										<NoStyleLink to="/movie">Populares</NoStyleLink>
+									</MenuItem>
+									<MenuItem>
+										<NoStyleLink to="/movie">Em cartaz</NoStyleLink>
+									</MenuItem>
+									<MenuItem>
+										<NoStyleLink to="/movie">Próximas Estreias</NoStyleLink>
+									</MenuItem>
+									<MenuItem>
+										<NoStyleLink to="/movie">Mais bem avaliados</NoStyleLink>
+									</MenuItem>
+								</Menu>
+							</Box>
+							<NoStyleLink to="/login">
+								<Button color="inherit">Entrar</Button>
+							</NoStyleLink>
+						</Toolbar>
 					</Container>
 				</CustomAppBar>
 			</HideOnScroll>
 			<Toolbar />
-			<Container disableGutters={true} >
-				{props.children}
-			</Container>
+			<Container disableGutters={true}>{props.children}</Container>
 		</>
 	);
 }
--- apps/themovie/src/components/MovieCard/MovieCard.js
+++ apps/themovie/src/components/MovieCard/MovieCard.js
@@ -12,14 +12,16 @@ import {
 import SimplePopover from './PopoverCard';
 
 const MovieCard = ({ poster_path, title, release_date }) => {
-	
 	return (
 		<CardStyle>
 			<Image>
 				<PopoverButton></PopoverButton>
 				<SimplePopover />
 				<Case>
-					<Poster src={`https://image.tmdb.org/t/p/w220_and_h330_face${poster_path}`} alt={`Poster ${title}`} />
+					<Poster
+						src={`https://image.tmdb.org/t/p/w220_and_h330_face${poster_path}`}
+						alt={`Poster ${title}`}
+					/>
 				</Case>
 			</Image>
 			<ContentCard>
