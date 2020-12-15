f1614d29c64140eddcec05d3675dace0e5dc8dbb
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-15 16:26:09 -0300

    prettier changes

--- apps/themovie/src/components/MovieCast/Scroller.jsx
+++ apps/themovie/src/components/MovieCast/Scroller.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import {
-  ScrollerWrap,
-	ScrollContent
-} from './style'
-import CastCard from './CastCard'
+import React from 'react';
+import { ScrollerWrap, ScrollContent } from './style';
+import CastCard from './CastCard';
 
-const Scroller = ({cast}) => {
+const Scroller = ({ cast }) => {
+	const renderCastCard = ({ name, character, gender, profile_path }, index) => {
+		return (
+			<CastCard
+				name={name}
+				character={character}
+				gender={gender}
+				profile_path={profile_path}
+				key={`cast-${index}`}
+			/>
+		);
+	};
 
-  const renderCastCard = ({name, character, gender, profile_path}, index) => {
-    return (
-      <CastCard name={name} character={character} gender={gender} profile_path={profile_path} key={`cast-${index}`} />
-    )
-  }
-
-  return (
-    <ScrollerWrap>
+	return (
+		<ScrollerWrap>
 			<ScrollContent>{cast.map(renderCastCard)}</ScrollContent>
 		</ScrollerWrap>
-  )
-}
+	);
+};
 
-export default Scroller
+export default Scroller;
--- apps/themovie/src/components/MovieCast/index.js
+++ apps/themovie/src/components/MovieCast/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import {
-	ContainerCast,
-} from './style';
+import { ContainerCast } from './style';
 import { Typography } from '@material-ui/core';
 import Scroller from './Scroller';
 
 const MovieCast = ({ cast }) => {
 	const hasCast = cast.length;
 
-	const renderScroller = (cast) => <Scroller cast={cast}/>
+	const renderScroller = cast => <Scroller cast={cast} />;
 
 	const renderNoCast = () => {
 		return (
@@ -16,12 +14,12 @@ const MovieCast = ({ cast }) => {
 				Não temos uma lista do elenco deste filme.
 			</Typography>
 		);
-	}
+	};
 
 	return (
 		<ContainerCast>
-				<Typography variant="h6">Elenco Principal</Typography>
-				{hasCast ? renderScroller(cast) : renderNoCast()}
+			<Typography variant="h6">Elenco Principal</Typography>
+			{hasCast ? renderScroller(cast) : renderNoCast()}
 		</ContainerCast>
 	);
 };
