0bb5dc52c6f71d96cc99053c51f6eaafd1d441ba
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-14 21:31:55 -0300

    movie Header refactored and add Director into it

--- apps/themovie/src/pages/Movie/Details/index.js
+++ apps/themovie/src/pages/Movie/Details/index.js
@@ -6,6 +6,8 @@ import { useParams } from 'react-router-dom';
 const Details = () => {
 	const { id } = useParams();
 	const [movieCast, setMovieCast] = useState([{}]);
+	const [movieCrew, setMovieCrew] = useState([{}]);
+	const [movie, setMovie] = useState([]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -13,14 +15,26 @@ const Details = () => {
 				.then(res => res.json())
 				.then(result => {
 					setMovieCast(result.cast);
+					setMovieCrew(result.crew);
 				});
 		};
 		fetchData();
 	}, [id]);
 
+	useEffect(() => {
+		const fetchMovie = async () => {
+			await fetch(`http://localhost:8080/movies/${id}?language=pt-BR`)
+				.then(res => res.json())
+				.then(result => {
+					setMovie(result);
+				});
+		};
+		fetchMovie();
+	}, [id]);
+
 	return (
 		<>
-			<MovieHeader />
+			<MovieHeader movie={movie} crew={movieCrew}/>
 			<MovieCast cast={movieCast} />
 		</>
 	);
