3443c95a3362d4b29fd1725add88186ef7e588d1
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-15 14:26:59 -0300

    component separation

---
+++ apps/themovie/src/components/MovieRecommendation/Scroller.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  ScrollContent,
+  ScrollerWrap
+} from './style'
+import RecommendationCard from './RecommendationCard'
+
+const Scroller = ({recommendation}) => {
+
+  const renderRecommendation = ({id, backdrop_path, title }, index) => <RecommendationCard key={`recommendation-${index}`} id={id} backdrop_path={backdrop_path} title={title}/>
+
+  return (
+    <ScrollerWrap>
+      <ScrollContent>
+        {recommendation.map(renderRecommendation)}
+      </ScrollContent>
+    </ScrollerWrap>
+  )
+}
+
+export default Scroller
