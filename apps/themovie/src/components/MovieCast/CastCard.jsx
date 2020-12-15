3123ff990a58a4fd3b948b49ceee44d968281d62
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-12-15 16:46:02 -0300

    prettier changes

--- apps/themovie/src/components/MovieCast/CastCard.jsx
+++ apps/themovie/src/components/MovieCast/CastCard.jsx
@@ -3,40 +3,39 @@ import { Typography } from '@material-ui/core';
 import manIcon from '../../assets/svg/man.svg';
 import womanIcon from '../../assets/svg/woman.svg';
 import {
-  BoxImage,
-  CastName,
-  CardCast,
-  CastImage,
-  CastCardContent
+	BoxImage,
+	CastName,
+	CardCast,
+	CastImage,
+	CastCardContent
 } from './style';
 
 const CastCard = ({ name, character, gender, profile_path }) => {
+	const renderCastImage = (profile_path, name) => (
+		<CastImage
+			image={`//image.tmdb.org/t/p/w138_and_h175_face/${profile_path}`}
+			title={name}
+		/>
+	);
 
-  const renderCastImage = (profile_path, name) => (
-    <CastImage
-      image={`//image.tmdb.org/t/p/w138_and_h175_face/${profile_path}`}
-      title={name}
-    />
-  );
+	const renderProfileHolder = gender => (
+		<BoxImage image={gender === 1 ? womanIcon : manIcon} />
+	);
 
-  const renderProfileHolder = gender => (
-    <BoxImage image={gender === 1 ? womanIcon : manIcon} />
-  );
-
-  const hasProfileImage = profile_path;
-  return (
-    <CardCast>
-      {hasProfileImage
-        ? renderCastImage(profile_path, name)
-        : renderProfileHolder(gender)}
-      <CastCardContent>
-        <CastName variant="body2">{name}</CastName>
-        <Typography variant="body2" color="textSecondary">
-          {character}
-        </Typography>
-      </CastCardContent>
-    </CardCast>
-  );
+	const hasProfileImage = profile_path;
+	return (
+		<CardCast>
+			{hasProfileImage
+				? renderCastImage(profile_path, name)
+				: renderProfileHolder(gender)}
+			<CastCardContent>
+				<CastName variant="body2">{name}</CastName>
+				<Typography variant="body2" color="textSecondary">
+					{character}
+				</Typography>
+			</CastCardContent>
+		</CardCast>
+	);
 };
 
 export default CastCard;
--- apps/themovie/src/components/MovieCast/Scroller.jsx
+++ apps/themovie/src/components/MovieCast/Scroller.jsx
@@ -3,7 +3,6 @@ import { ScrollerWrap, ScrollContent } from './style';
 import CastCard from './CastCard';
 
 const Scroller = ({ cast }) => {
-
 	const renderCastCard = ({ name, character, gender, profile_path }, index) => {
 		return (
 			<CastCard
